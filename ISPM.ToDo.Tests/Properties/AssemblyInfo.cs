c51c80d2b62c3de6f1b6711e7cf77a4352eda2de
Rodrigo Braga <rodrigobraga@msn.com>
2012-12-29 12:13:10 -0200

    arquivos de controle do Visual Studio

--- ISPM.ToDo.Tests/Properties/AssemblyInfo.cs
+++ ISPM.ToDo.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ISPM.ToDo.Tests")]
+[assembly: AssemblyTitle("ISPM.Todo.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ISPM.ToDo.Tests")]
+[assembly: AssemblyProduct("ISPM.Todo.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
