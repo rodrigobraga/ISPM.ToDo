8f23e03ff3563d697d8562e9c6476db43c2c2340
Rodrigo Braga <rodrigobraga@msn.com>
2012-12-29 14:41:25 -0200

    inclusao da API

---
+++ ISPM.ToDo.API/Controllers/ToDoController.cs
@@ -0,0 +1,35 @@
+﻿namespace ISPM.ToDo.API.Controllers
+{
+    using System.Collections.Generic;
+    using System.Web.Http;
+
+    public class ToDoController : ApiController
+    {
+        // GET api/todo
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/todo/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/todo
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/todo/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/todo/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
