b0500eb268a913e5f15dac6ff561db9f46300f78
Rodrigo Braga <rodrigobraga@msn.com>
2012-12-29 13:56:26 -0200

    inclusão do repositorio

---
+++ ISPM.ToDo.Data/Repositories/ToDoRepository.cs
@@ -0,0 +1,29 @@
+﻿namespace ISPM.ToDo.Data.Repositories
+{
+    using ISPM.ToDo.Data.Infrastructure;
+    using ISPM.ToDo.Domain.Entities;
+
+    /// <summary>
+    /// The ToDoRepository interface.
+    /// </summary>
+    public interface IToDoRepository : IRepository<ToDo>
+    {
+    }
+
+    /// <summary>
+    /// The to do repository.
+    /// </summary>
+    public class ToDoRepository : RepositoryBase<ToDo>, IToDoRepository
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ToDoRepository"/> class.
+        /// </summary>
+        /// <param name="factory">
+        /// The factory.
+        /// </param>
+        public ToDoRepository(IDatabaseFactory factory)
+            : base(factory)
+        {
+        }
+    }
+}
